fffb5ce9fe6b034fd85104f04840b34fa81f76b8
Dylan Spin <dylanspin100@hotmail.com>
2021-02-09 14:15:49 +0100

    Begin Cheat sheet Site
    Begrijp redelijk wat ga nu een cheat sheet website voor me zelf maken voor het oefenen

--- First/src/Controller/MainController.php
+++ First/src/Controller/MainController.php
@@ -3,18 +3,41 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/", name="Home")
      */
-    public function index(): Response
+
+    public function index()
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+        return $this->render('home/index.html.twig');
+    }
+
+    /**
+     * @Route("/custom/{name?}", name="custom")
+     * @param Request $request
+     * @return Response
+     */
+    public function custom(Request $request)
+    {
+        $name = $request->get('name');
+        return $this->render('home/custom.html.twig',[
+            'name' => $name
+        ]);
+    }
+
+    /**
+     * @Route("/somthing", name="somthing")
+     */
+    public function somthing()
+    {
+        return $this->render('home/somthing.html.twig',[
+           
         ]);
     }
 }
